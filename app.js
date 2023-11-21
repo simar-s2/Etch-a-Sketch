c7f3a4379722243fdbfa0983ce78c9a3d9812b74
simar-s2 <simarjot2468@gmail.com>
2023-11-21 19:51:21 +0300

    Basic sketching working

--- app.js
+++ app.js
@@ -7,5 +7,10 @@ grid.style.gridTemplateRows = `repeat(16, 1fr)`;
 for (let i = 0; i < 256; i++) {
     let pixel = document.createElement('div');
     pixel.style.backgroundColor = '#000000';
+    pixelHeight = 500 / 16
+    pixel.style.height = `${pixelHeight}px`
     grid.appendChild(pixel);
+    pixel.addEventListener('mouseover', () => {
+        pixel.style.backgroundColor = '#ffffff'
+    })
 }
