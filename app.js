64ca4485c398e84ac37c9274d993eebb7a34d2c7
simar-s2 <simarjot2468@gmail.com>
2023-11-23 02:23:46 +0300

    Add clear button feature

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 const grid = document.querySelector('.grid');
 const clearbtn = document.querySelector('.clearbtn');
-let size = 16;
+const form = document.querySelector('.resizeform')
 
-function clearGrid (size) {
-
-    grid.style.gridTemplateColumns = `repeat(16, 1fr)`;
-    grid.style.gridTemplateRows = `repeat(16, 1fr)`;
+function createGrid (size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     for (let i = 0; i < size*size; i++) {
         let pixel = document.createElement('div');
-        pixel.style.backgroundColor = '#000000';
+        pixel.classList.add("pixel")
+        // let pixels = grid.querySelectorAll('div');
+        // pixels.forEach((div) => div.remove());
+        pixel.style.backgroundColor = '#000';
         pixelHeight = 700 / size;
-        pixel.style.height = `${pixelHeight}px`
+        pixel.style.height = `${pixelHeight}px`;
         grid.appendChild(pixel);
         pixel.addEventListener('mouseover', () => {
-            pixel.style.backgroundColor = '#ffffff'
+            pixel.style.backgroundColor = '#fff';
         })
     }
 }
 
-clearGrid(size);
+clearbtn.addEventListener('click', () => {
+    let pixels = grid.querySelectorAll('div');
+    pixels.forEach((div) => div.style.backgroundColor = '#000')
+})
+
+createGrid(16);
